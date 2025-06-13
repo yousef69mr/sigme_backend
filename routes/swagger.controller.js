803f4c460a342a476afc05ad6269af66d3a0ada4
yousef69mr <youseffouad69@gmail.com>
2025-06-13 16:31:32 +0200

    Fix Swagger UI MIME issue for Vercel

--- routes/swagger.controller.js
+++ routes/swagger.controller.js
@@ -2,12 +2,11 @@
 
 import express from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
-import { readFileSync } from 'fs';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-// import { serveStatic } from 'serve-static';
-import swaggerUiDist from 'swagger-ui-dist';
+
 import { PORT } from '../lib/constants/config.js'
 
 const __filename = fileURLToPath(import.meta.url);
@@ -47,35 +46,34 @@ const swaggerSpec = swaggerJsdoc({
     ], // path to files with JSDoc comments
 });
 
-router.use('/swagger-ui', express.static(swaggerUiDist.getAbsoluteFSPath()));
-
 router.get('/api-docs', (req, res) => {
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>API Docs</title>
-      <link rel="stylesheet" href="/swagger-ui/swagger-ui.css" />
-    </head>
-    <body>
-      <div id="swagger-ui"></div>
-      <script src="/swagger-ui/swagger-ui-bundle.js"></script>
-      <script src="/swagger-ui/swagger-ui-standalone-preset.js"></script>
-      <script>
-        window.onload = function() {
-          SwaggerUIBundle({
-            url: '/swagger.json',
-            dom_id: '#swagger-ui',
-            presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-            layout: 'StandaloneLayout'
-          });
-        };
-      </script>
-    </body>
-    </html>
-  `;
-  res.send(html);
+    const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>API Docs</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+  <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
+  <script>
+    window.onload = function() {
+      SwaggerUIBundle({
+        url: '/swagger.json',
+        dom_id: '#swagger-ui',
+        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+        layout: 'StandaloneLayout'
+      });
+    };
+  </script>
+</body>
+</html>
+`;
+
+    res.send(html);
 });
 
 
